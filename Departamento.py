44c1c0747d8dec08fa65c9dd77833050f76906f4
keisylamanna <keisy.lamanna@correo.unimet.edu.ve>
2025-08-08 19:39:16 -0400

    Update Departamento.py

--- Departamento.py
+++ Departamento.py
@@ -1,16 +1,13 @@
+from utils import utils
 import requests
-import time
 
 class Departamento:
-    def __init__(self,department):
-        self.department = department
-    
-    def show(self):
-        print(f"Departamentos: ")
-        for department in self.department:
-            print(f"    {department}")
-
     def search():
+
+        """
+        Muestra las obras por Departamento, el cual es escogido por el usuario
+        """
+
         objects_link = "https://collectionapi.metmuseum.org/public/collection/v1/objects"
         search_link = "https://collectionapi.metmuseum.org/public/collection/v1/search"
         departments_link = "https://collectionapi.metmuseum.org/public/collection/v1/departments"
@@ -19,33 +16,39 @@ class Departamento:
             data = requests.get(departments_link)
             data = data.json()
 
-            print(f"Departamentos: ")
-            for departamento in data["departments"]:
-                print(f"    {departamento["departmentId"]} - {departamento["displayName"]}")
-                print()
-                
-        
+            department_ids = []
 
-               
+            print(f"Departamentos: ")
+            print()
+
+            for departamento in data["departments"]:
+                department_ids.append(departamento["departmentId"])
+                print(f"        {departamento["departmentId"]} - {departamento["displayName"]}")
+
            
             while True: 
 
-                department_input = input("""Introduzca el ID del departamento:
-                --> """)
-    
+                print()
+                department_input = input("""Introduzca el ID del departamento o marque 0 para volver:
+        --> """)
+                
+                if department_input == "0":
+                    break
 
+                elif int(department_input) not in department_ids:
+                    print()
+                    print("El departamento introducido no se encuentra en la lista")
+                    continue
+    
                 params = {
                     "q": "",
                     "departmentId": int(department_input),
-                    "hasImages":"true"
                 }
                 try:
                     data = requests.get(search_link,params=params)
                     data = data.json()
-                except ValueError or KeyError or TypeError:
 
-                
-
+                except (ValueError, KeyError, TypeError):
                     print()
                     print(f"{data['total']} objetos encontrados")
 
@@ -54,11 +57,8 @@ class Departamento:
                 for id in data["objectIDs"]:
                     objects.append(id)
 
-                    
-                    
-
                 print("""
-        Obras:
+Obras:
         """)
                 for i in range(len(objects)):
 
@@ -67,18 +67,18 @@ class Departamento:
                         object_data = requests.get(object_link)
                         object_data = object_data.json()
 
-                        print(f"{i+1}.- {object_data["objectID"]}, {object_data["title"]}, {object_data["artistDisplayName"]}")
+                        print(f"        {object_data["objectID"]}, {object_data["title"]}, {object_data["artistDisplayName"]}")
 
-                    except ValueError or KeyError or TypeError:
+                    except (ValueError, KeyError, TypeError):
                         print("No se pudo obtener este item")
 
                         while True:
                             print()
                             seguir = input("""Quiere seguir mostrando obras?
-                1. Si
-                2. Ver obra
-                3. No
-            --> """)
+            1. Si
+            2. Ver obra
+            3. No
+        --> """)
 
                             if seguir == "1":
                                 print()
@@ -87,7 +87,12 @@ class Departamento:
                             elif seguir == "2":
                                 print()
                                 ver = input("""Introduzca el ID de la obra para ver su informacion:        
-            --> """)
+        --> """)
+                                
+                                if int(ver) not in objects:
+                                    print()
+                                    print("El ID del objeto no se encuentra en la lista de obras")
+                                    continue
 
                                 try:
                                     object_link = f"{objects_link}/{ver}"
@@ -97,35 +102,21 @@ class Departamento:
                                     print()
 
                                     print(f"""'{object_data["title"]}' by {object_data["artistDisplayName"]},
-        {object_data["artistNationality"]}, {object_data["artistBeginDate"]} - {object_data["artistEndDate"]}
-        {object_data["classification"]}, {object_data["objectDate"]}
-        {object_data["primaryImageSmall"]}""")
+{object_data["artistNationality"]}, {object_data["artistBeginDate"]} - {object_data["artistEndDate"]}
+{object_data["classification"]}, {object_data["objectDate"]}
+{object_data["primaryImageSmall"]}""")
 
-                                except ValueError or KeyError or TypeError:
+                                except (ValueError, KeyError, TypeError):
                                     print()
                                     print("No se puede mostrar la obra en estos momentos, intente nuevamente")
 
                                     while True:
                                         try_again = input("""
-                1. Intentar de nuevo
-                2. Salir                
-            --> """)
+            1. Intentar de nuevo
+            2. Salir                
+        --> """)
                                         if try_again == "1":
-                                            try:
-                                                object_link = f"{objects_link}/{ver}"
-                                                object_data = requests.get(object_link)
-                                                object_data = object_data.json()
-
-                                                print()
-                                                print(f"""'{object_data["title"]}' by {object_data["artistDisplayName"]},
-                    {object_data["artistNationality"]}, {object_data["artistBeginDate"]} - {object_data["artistEndDate"]}
-                    {object_data["classification"]}, {object_data["objectDate"]}
-                    {object_data["primaryImageSmall"]}""")
-                                                break
-
-                                            except ValueError or KeyError or TypeError:
-                                                print()
-                                                print("No se puede mostrar la obra en estos momentos, intente nuevamente")
+                                            continue
                                         elif try_again == "2":
                                             break
                                         else:
@@ -143,8 +134,16 @@ class Departamento:
 
                 while True:
                     print()
-                    ver = input("""Introduzca el ID de la obra para ver su informacion:        
-            --> """)
+                    ver = input("""Introduzca el ID de la obra para ver su informacion o marque 0 para volver:        
+        --> """)
+                    
+                    if ver == "0":
+                        break
+
+                    elif int(ver) not in objects:
+                        print()
+                        print("El ID del objeto no se encuentra en la lista de obras")
+                        continue
 
                     try:
                         object_link = f"{objects_link}/{ver}"
@@ -153,19 +152,33 @@ class Departamento:
 
                         print()
                         print(f"""'{object_data["title"]}' by {object_data["artistDisplayName"]},
-            {object_data["artistNationality"]}, {object_data["artistBeginDate"]} - {object_data["artistEndDate"]}
-            {object_data["classification"]}, {object_data["objectDate"]}
-            {object_data["primaryImageSmall"]}""")
+{object_data["artistNationality"]}, {object_data["artistBeginDate"]} - {object_data["artistEndDate"]}
+{object_data["classification"]}, {object_data["objectDate"]}
+{object_data["primaryImageSmall"]}""")
+                        
+                        while True:
+                            image = input("""Desea ver la imagen?
+            1. Si
+            2. No
+        --> """)
+                            if image == "1":
+                                utils.guardar_y_mostrar_imagen(object_data["primaryImageSmall"],f"{object_data["objectID"]}")
+                                break
 
-                    except ValueError or KeyError or TypeError:
+                            elif image == "2":
+                                break
+                            else:
+                                print("Opcion Invalida")
+
+                    except (ValueError, KeyError, TypeError):
                         print()
                         print("No se puede mostrar la obra en estos momentos, intente nuevamente")
 
                         while True:
                             try_again = input("""
-                1. Intentar de nuevo
-                2. Salir                 
-            --> """)
+            1. Intentar de nuevo
+            2. Salir                 
+        --> """)
                             if try_again == "1":
                                 try:
                                     object_link = f"{objects_link}/{ver}"
@@ -174,29 +187,34 @@ class Departamento:
 
                                     print()
                                     print(f"""'{object_data["title"]}' by {object_data["artistDisplayName"]},
-            {object_data["artistNationality"]}, {object_data["artistBeginDate"]} - {object_data["artistEndDate"]}
-            {object_data["classification"]}, {object_data["objectDate"]}
-            {object_data["primaryImageSmall"]}""")
-                                    break
+{object_data["artistNationality"]}, {object_data["artistBeginDate"]} - {object_data["artistEndDate"]}
+{object_data["classification"]}, {object_data["objectDate"]}
+{object_data["primaryImageSmall"]}""")
+                                    
+                                    while True:
+                                        image = input("""Desea ver la imagen?
+            1. Si
+            2. No
+        --> """)
+                                        if image == "1":
+                                            utils.guardar_y_mostrar_imagen(object_data["primaryImageSmall"],f"{object_data["objectID"]}")
+                                            break
 
-                                except ValueError or KeyError or TypeError:
+                                        elif image == "2":
+                                            break
+                                        else:
+                                            print("Opcion Invalida")
+
+                                except (ValueError, KeyError, TypeError):
                                     print()
                                     print("No se puede mostrar la obra en estos momentos, intente nuevamente")
                             elif try_again == "2":
                                 return
                             else:
                                 print("Opcion invalida")
+                else:
+                    break
                 
-        except ValueError or KeyError or TypeError:
+        except (ValueError, KeyError, TypeError):
             print("Error con los servidores")
-
-    
-
-    
-
-
-                    
-            
-        
-            
                     
