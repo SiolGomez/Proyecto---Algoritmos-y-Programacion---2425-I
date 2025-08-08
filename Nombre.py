1ce0bac9ff046989ea78a1b933808783aeb42345
keisylamanna <keisy.lamanna@correo.unimet.edu.ve>
2025-08-08 19:37:38 -0400

    Update Nombre.py

--- Nombre.py
+++ Nombre.py
@@ -1,14 +1,16 @@
+from utils import utils
 import requests
 
-
 class Nombre:
-    def __init__(self,name):
-        self.name = name
 
     def search():
+
+        """
+        Muestra las obras por Nombre, el cual es introducido por el usuario
+        """
+
         objects_link = "https://collectionapi.metmuseum.org/public/collection/v1/objects"
         search_link = "https://collectionapi.metmuseum.org/public/collection/v1/search"
-        
         
         name_input = input("""Introduzca el nombre del autor:
         --> """)
@@ -31,8 +33,8 @@ class Nombre:
                 objects.append(id)
 
             print("""
-    Obras:
-    """)
+Obras:
+""")
             
             for i in range(len(objects)):
 
@@ -41,9 +43,11 @@ class Nombre:
                     object_data = requests.get(object_link)
                     object_data = object_data.json()
 
+                    if name_input.lower() in object_data["artistDisplayName"].lower():
                    
-                    print(f"{i+1}.- {object_data['objectID']},{object_data['title']},{object_data['artistDisplayName']}")
-                except ValueError or KeyError:
+                        print(f"        {object_data['objectID']}, {object_data['title']}, {object_data['artistDisplayName']}")
+                
+                except (ValueError, KeyError, TypeError):
                     print("No se pudo obtener este item")
 
                     while True:
@@ -71,11 +75,25 @@ class Nombre:
                                 print()
 
                                 print(f"""'{object_data["title"]}' by {object_data["artistDisplayName"]},
-    {object_data["artistNationality"]}, {object_data["artistBeginDate"]} - {object_data["artistEndDate"]}
-    {object_data["classification"]}, {object_data["objectDate"]}
-    {object_data["primaryImageSmall"]}""")
+{object_data["artistNationality"]}, {object_data["artistBeginDate"]} - {object_data["artistEndDate"]}
+{object_data["classification"]}, {object_data["objectDate"]}
+{object_data["primaryImageSmall"]}""")
+                                
+                                while True:
+                                            image = input("""Desea ver la imagen?
+            1. Si
+            2. No
+        --> """)
+                                            if image == "1":
+                                                utils.guardar_y_mostrar_imagen(object_data["primaryImageSmall"],f"{object_data["objectID"]}")
+                                                break
 
-                            except ValueError or KeyError:
+                                            elif image == "2":
+                                                break
+                                            else:
+                                                print("Opcion Invalida")
+
+                            except (ValueError, KeyError, TypeError):
                                 print()
                                 print("No se puede mostrar la obra en estos momentos, intente nuevamente")
 
@@ -92,12 +110,25 @@ class Nombre:
 
                                             print()
                                             print(f"""'{object_data["title"]}' by {object_data["artistDisplayName"]},
-                {object_data["artistNationality"]}, {object_data["artistBeginDate"]} - {object_data["artistEndDate"]}
-                {object_data["classification"]}, {object_data["objectDate"]}
-                {object_data["primaryImageSmall"]}""")
-                                            break
+{object_data["artistNationality"]}, {object_data["artistBeginDate"]} - {object_data["artistEndDate"]}
+{object_data["classification"]}, {object_data["objectDate"]}
+{object_data["primaryImageSmall"]}""")
+                                            
+                                            while True:
+                                                    image = input("""Desea ver la imagen?
+            1. Si
+            2. No
+        --> """)
+                                                    if image == "1":
+                                                        utils.guardar_y_mostrar_imagen(object_data["primaryImageSmall"],f"{object_data["objectID"]}")
+                                                        break
 
-                                        except ValueError or KeyError:
+                                                    elif image == "2":
+                                                        break
+                                                    else:
+                                                        print("Opcion Invalida")
+
+                                        except (ValueError, KeyError, TypeError):
                                             print()
                                             print("No se puede mostrar la obra en estos momentos, intente nuevamente")
                                     elif try_again == "2":
@@ -117,8 +148,11 @@ class Nombre:
 
             while True:
                 print()
-                ver = input("""Introduzca el ID de la obra para ver su informacion:        
+                ver = input("""Introduzca el ID de la obra para ver su informacion o marque 0 para volver:        
         --> """)
+                
+                if ver == "0":
+                    break
 
                 try:
                     object_link = f"{objects_link}/{ver}"
@@ -127,11 +161,25 @@ class Nombre:
 
                     print()
                     print(f"""'{object_data["title"]}' by {object_data["artistDisplayName"]},
-        {object_data["artistNationality"]}, {object_data["artistBeginDate"]} - {object_data["artistEndDate"]}
-        {object_data["classification"]}, {object_data["objectDate"]}
-        {object_data["primaryImageSmall"]}""")
+{object_data["artistNationality"]}, {object_data["artistBeginDate"]} - {object_data["artistEndDate"]}
+{object_data["classification"]}, {object_data["objectDate"]}
+{object_data["primaryImageSmall"]}""")
+                    
+                    while True:
+                        image = input("""Desea ver la imagen?
+            1. Si
+            2. No
+        --> """)
+                        if image == "1":
+                            utils.guardar_y_mostrar_imagen(object_data["primaryImageSmall"],f"{object_data["objectID"]}")
+                            break
 
-                except ValueError or KeyError:
+                        elif image == "2":
+                            break
+                        else:
+                            print("Opcion Invalida")
+
+                except (ValueError, KeyError, TypeError):
                     print()
                     print("No se puede mostrar la obra en estos momentos, intente nuevamente")
 
@@ -148,12 +196,25 @@ class Nombre:
 
                                 print()
                                 print(f"""'{object_data["title"]}' by {object_data["artistDisplayName"]},
-        {object_data["artistNationality"]}, {object_data["artistBeginDate"]} - {object_data["artistEndDate"]}
-        {object_data["classification"]}, {object_data["objectDate"]}
-        {object_data["primaryImageSmall"]}""")
-                                break
+{object_data["artistNationality"]}, {object_data["artistBeginDate"]} - {object_data["artistEndDate"]}
+{object_data["classification"]}, {object_data["objectDate"]}
+{object_data["primaryImageSmall"]}""")
+                                
+                                while True:
+                                    image = input("""Desea ver la imagen?
+            1. Si
+            2. No
+        --> """)
+                                    if image == "1":
+                                        utils.guardar_y_mostrar_imagen(object_data["primaryImageSmall"],f"{object_data["objectID"]}")
+                                        break
 
-                            except ValueError or KeyError:
+                                    elif image == "2":
+                                        break
+                                    else:
+                                        print("Opcion Invalida")
+
+                            except (ValueError, KeyError, TypeError):
                                 print()
                                 print("No se puede mostrar la obra en estos momentos, intente nuevamente")
                         elif try_again == "2":
@@ -161,7 +222,5 @@ class Nombre:
                         else:
                             print("Opcion invalida")
             
-        except ValueError or KeyError:
-            print("Error con los servidores")    
-
-    search()
+        except (ValueError, KeyError, TypeError):
+            print("Error con los servidores")
